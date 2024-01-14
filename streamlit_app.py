6b0df0eb8d5531fcf1d722d2bd476dcbdf8d2f55
PHILIPSNOWFLAKE <156471161+PHILIPSNOWFLAKE@users.noreply.github.com>
2024-01-14 19:49:07 +1300

    Using Variable to Call API

--- streamlit_app.py
+++ streamlit_app.py
@@ -21,10 +21,12 @@ fruits_to_show = my_fruit_list.loc[fruits_selected]
 # Display the table on the page.
 streamlit.dataframe(fruits_to_show)
 
-
-fruityvice_response = requests.get("https://fruityvice.com/api/fruit/"+"Kiwi")
-
 streamlit.header("Fruityvice Fruit Advice!")
+
+fruit_choice = streamlit.text_input('What fruit would you like information about?','Kiwi')
+streamlit.write('The user entered ', fruit_choice)
+
+fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + fruit_choice)
 
 # write your own comment - Normalises the JSON into a DataFrame
 fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
